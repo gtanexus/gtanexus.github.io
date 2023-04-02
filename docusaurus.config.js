6dfa52aee01a3dcf253229561f41c8b8e4d7d2b7
OG <94694368+zZOGZz@users.noreply.github.com>
2023-04-02 17:53:21 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,7 +56,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/docusaurus-social-card.png',
       navbar: {
         title: 'Neptuno RP',
         logo: {
@@ -75,7 +75,7 @@ const config = {
             position: 'right',
           },
           {
-            href: 'https://neptunorp.tebex.io',
+            href: 'https://neptunorp.com',
             label: 'Tienda',
             position: 'right',
           },
