3ffe0aad338461503198821e9bfd701dd9c8ea5b
OG <94694368+zZOGZz@users.noreply.github.com>
2023-11-14 05:41:42 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.neptunorp.com',
+  url: 'https://docs.gtanexus.gg',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -75,7 +75,7 @@ const config = {
             position: 'right',
           },
           {
-            href: 'https://neptunorp.com',
+            href: 'https://gtanexus.gg',
             label: 'Tienda',
             position: 'right',
           },
